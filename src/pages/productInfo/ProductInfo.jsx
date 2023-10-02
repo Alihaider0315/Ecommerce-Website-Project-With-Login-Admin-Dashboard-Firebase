ce59c64228e314a729982ccadc2830beb1049549
Alihaider0315 <ah712200@gmail.com>
2023-10-02 12:03:37 +0500

    code updated

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -177,7 +177,7 @@ function ProductInfo() {
 
                             <div className="flex">
                                 <span className="title-font font-medium text-2xl text-gray-900">
-                                ₹{products.price}
+                                PKR {products.price}
                                 </span>
                                 <button  onClick={()=>addCart(products)} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                                     Add To Cart
