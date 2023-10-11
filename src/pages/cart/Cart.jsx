f235b4cc4c0bba491860f20e1f0932e317ed7e8e
Alihaider0315 <ah712200@gmail.com>
2023-10-11 17:20:48 +0500

    code updated.

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -28,7 +28,7 @@ function Cart() {
   useEffect(() => {
     let temp = 0;
     cartItems.forEach((cartItem) => {
-      temp = temp + parseInt(cartItem.price);
+      temp += parseInt(cartItem.price) * cartItem.quantity;
     });
     setTotalAmount(temp);
   }, [cartItems]);
@@ -101,55 +101,120 @@ function Cart() {
         <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
         <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
-            {cartItems.map((item, index) => {
-              const { title, price, description, imageUrl } = item;
-              return (
-                <div className="justify-between mb-6 rounded-lg border drop-shadow-xl bg-white p-6 sm:flex sm:justify-start" style={{ backgroundColor: mode === 'dark' ? 'rgb(32 33 34)' : '', color: mode === 'dark' ? 'white' : '' }}>
-                  <img src={imageUrl} alt="product-image" className="w-full rounded-lg sm:w-40" />
-                  <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                    <div className="mt-5 sm:mt-0">
-                      <h2 className="text-lg font-bold text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>{title}</h2>
-                      <h2 className="text-sm text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>{description}</h2>
-                      <p className="mt-1 text-xs font-semibold text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>PKR {price}</p>
-                    </div>
-                    <div onClick={() => deleteCart(item)} className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                      </svg>
+            {cartItems.length === 0 ? (
+              <p className="text-center">Your cart is empty.</p>
+            ) : (
+              cartItems.map((item, index) => {
+                const { title, price, description, imageUrl } = item;
+                return (
+                  <div
+                    className="justify-between mb-6 rounded-lg border drop-shadow-xl bg-white p-6 sm:flex sm:justify-start"
+                    style={{ backgroundColor: mode === 'dark' ? 'rgb(32 33 34)' : '', color: mode === 'dark' ? 'white' : '' }}
+                    key={index}
+                  >
+                    <img src={imageUrl} alt="product-image" className="w-full rounded-lg sm:w-40" />
+                    <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                      <div className="mt-5 sm:mt-0">
+                        <h2 className="text-lg font-bold text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                          {title}
+                        </h2>
+                        <h2 className="text-sm text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                          {description}
+                        </h2>
+                        <p className="mt-1 text-xs font-semibold text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                          PKR {price}
+                        </p>
+                      </div>
+                      <div
+                        onClick={() => deleteCart(item)}
+                        className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6"
+                      >
+                        <div className="flex items-center">
+                          <button
+                            onClick={() => {
+                              // Decrease quantity
+                              if (item.quantity > 1) {
+                                dispatch(decreaseQuantity(item));
+                              }
+                            }}
+                            className="text-gray-500"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M20 12H4" />
+                            </svg>
+                          </button>
+                          <span className="mx-2">{item.quantity}</span>
+                          <button
+                            onClick={() => {
+                              // Increase quantity
+                              dispatch(increaseQuantity(item));
+                            }}
+                            className="text-gray-500"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+                            </svg>
+                          </button>
+                        </div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })
+            )}
           </div>
 
           <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3" style={{ backgroundColor: mode === 'dark' ? 'rgb(32 33 34)' : '', color: mode === 'dark' ? 'white' : '' }}>
             <div className="mb-2 flex justify-between">
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Subtotal</p>
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>PKR {totalAmount}</p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                Subtotal
+              </p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                PKR {totalAmount}
+              </p>
             </div>
             <div className="flex justify-between">
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Shipping</p>
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>PKR {shipping}</p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                Shipping
+              </p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                PKR {shipping}
+              </p>
             </div>
             <hr className="my-4" />
             <div className="flex justify-between mb-3">
-              <p className="text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Total</p>
+              <p className="text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                Total
+              </p>
               <div className>
-                <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>PKR {grandTotal}</p>
+                <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                  PKR {grandTotal}
+                </p>
               </div>
             </div>
-            <Modal
-              name={name}
-              address={address}
-              pincode={pincode}
-              phoneNumber={phoneNumber}
-              setName={setName}
-              setAddress={setAddress}
-              setPincode={setPincode}
-              setPhoneNumber={setPhoneNumber}
-              buyNow={buyNow}
-            />
+            {cartItems.length > 0 && ( // Render the Buy Now button only when the cart is not empty
+              <Modal
+                name={name}
+                address={address}
+                pincode={pincode}
+                phoneNumber={phoneNumber}
+                setName={setName}
+                setAddress={setAddress}
+                setPincode={setPincode}
+                setPhoneNumber={setPhoneNumber}
+                buyNow={buyNow}
+              />
+            )}
           </div>
         </div>
       </div>
--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,14 +1,14 @@
-import { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import myContext from '../../context/data/myContext'
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import myContext from '../../context/data/myContext';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../fireabase/FirebaseConfig';
 import { toast } from 'react-toastify';
 import Loader from '../../components/loader/Loader';
 
 function Login() {
-    const context = useContext(myContext)
-    const {loading, setLoading} = context;
+    const context = useContext(myContext);
+    const { loading, setLoading } = context;
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -16,9 +16,9 @@ function Login() {
     const navigate = useNavigate();
 
     const login = async () => {
-        setLoading(true)
+        setLoading(true);
         try {
-            const result = await signInWithEmailAndPassword(auth,email,password);
+            const result = await signInWithEmailAndPassword(auth, email, password);
             toast.success("Login successful", {
                 position: "top-right",
                 autoClose: 2000,
@@ -28,31 +28,40 @@ function Login() {
                 draggable: true,
                 progress: undefined,
                 theme: "colored",
-              })
-            localStorage.setItem('user', JSON.stringify(result))
-            navigate('/')
-            setLoading(false)
-            
+            });
+            localStorage.setItem('user', JSON.stringify(result));
+            navigate('/');
+            setLoading(false);
         } catch (error) {
-            toast.error(error)
-            setLoading(loading)
+            const errorMessage = error.message; // Extract the error message
+            toast.error(errorMessage, {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+            setLoading(false); // Set loading to false
         }
-
     }
-   
+
     return (
-        <div className=' flex justify-center items-center h-screen'>
-            {loading && <Loader/>}
-            <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
+        <div className='flex justify-center items-center h-screen'>
+            {loading && <Loader />}
+            <div className='bg-gray-800 px-10 py-10 rounded-xl'>
                 <div className="">
                     <h1 className='text-center text-white text-xl mb-4 font-bold'>Login</h1>
                 </div>
                 <div>
-                    <input type="email"
-                    value={email}
-                    onChange={(e)=> setEmail(e.target.value)}
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         name='email'
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                        className='bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
                 </div>
@@ -60,24 +69,24 @@ function Login() {
                     <input
                         type="password"
                         value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                        onChange={(e) => setPassword(e.target.value)}
+                        className='bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
-                <div className=' flex justify-center mb-3'>
+                <div className='flex justify-center mb-3'>
                     <button
-                    onClick={login}
-                        className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
+                        onClick={login}
+                        className='bg-yellow-500 w-full text-black font-bold px-2 py-2 rounded-lg'>
                         Login
                     </button>
                 </div>
                 <div>
-                    <h2 className='text-white'>Don't have an account <Link className=' text-yellow-500 font-bold' to={'/signup'}>Signup</Link></h2>
+                    <h2 className='text-white'>Don't have an account <Link className='text-yellow-500 font-bold' to={'/signup'}>Signup</Link></h2>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Login
+export default Login;
